2d44babc5b5fcaa854082d64f1c40d136cd85e43
aldae7 <aldinofajrian2@mhs.pelitabangsa.ac.id>
2018-10-03 00:07:54 +0700

    Praktikum.cbp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int p,l,keliling;
+    cout << "masukkan panjang persegi panjang= ";
+    cin >> p;
+    cout << "masukkan lebar persegi panjang= ";
+    cin >> l;
+
+    keliling - p*l;
+    cout << "keliling persegi panjang adalah " << keliling;
+    return 0;
+}
+# Praktikum1
